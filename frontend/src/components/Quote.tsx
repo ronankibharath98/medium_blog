c1980af247ca693881a94d271181266e104a37fd
nani <ronankibharath1998@gmail.com>
2024-10-11 22:55:28 -0400

    Finished all the backend and initialised front end

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,21 @@
+export const Quote = () => {
+    return (
+        <div className="bg-slate-200 h-screen flex flex-col justify-center">
+            <div className="flex items-center">
+                <div className="bg-red-50">
+                    <div className="text-3xl font-semibold">
+                        "The Customer Service I recieved was<br />exceptional. The support team went above<br />
+                        and beyond to address my concerns"
+                    </div>
+                    <div className="text-gray-950 font-semibold text-xl">
+                        Jules Winnfield
+                    </div>
+                    <div className="max-w-md">
+                        CEO, Acme Inc
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,14 @@
+import { Quote } from "../components/Quote"
+
+export const Signup = () => {
+    return (
+        <div className="grid grid-cols-2">
+            <div>
+
+            </div>
+            <div>
+                <Quote />
+            </div>
+        </div>
+    )
+}
