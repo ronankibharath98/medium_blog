4af60c88b904fdb3ee07254de5788c1b0b51e686
nani <ronankibharath1998@gmail.com>
2024-10-16 22:25:44 -0400

    fixed cors error to allow origin vercel

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,7 @@ const app = new Hono()
 app.use('/*', cors())
 app.use('/*',
     cors({
-      origin: 'http://localhost:5173',
+      origin: 'https://medium-blog-eight-green.vercel.app',
       allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests'],
       allowMethods: ['POST', 'GET', 'OPTIONS'],
       exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
